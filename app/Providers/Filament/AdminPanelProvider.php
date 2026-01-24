e342f027815ebeef4fd442c050f22ada04c29869
adammcadam <adam.wragg@hotmail.co.uk>
2026-01-24 14:52:18 +0000

    Added ability to switch org if the user belongs to multiple. Plus quick UX to show the current organisation the user is bound to

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -3,6 +3,9 @@
 namespace App\Providers\Filament;
 
 use App\Http\Middleware\EnsureActiveOrganisation;
+use App\Services\OrganisationContext;
+use Filament\Actions\Action;
+use Filament\Forms\Components\Select;
 use Filament\Http\Middleware\Authenticate;
 use Filament\Http\Middleware\AuthenticateSession;
 use Filament\Http\Middleware\DisableBladeIconComponents;
@@ -11,6 +14,7 @@ use Filament\Pages\Dashboard;
 use Filament\Panel;
 use Filament\PanelProvider;
 use Filament\Support\Colors\Color;
+use Filament\Support\Icons\Heroicon;
 use Filament\Widgets\AccountWidget;
 use Filament\Widgets\FilamentInfoWidget;
 use Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse;
@@ -55,6 +59,42 @@ class AdminPanelProvider extends PanelProvider
             ])
             ->authMiddleware([
                 Authenticate::class,
+            ])
+            ->userMenuItems([
+                Action::make('currentOrganization')
+                    ->label(fn () =>
+                        app(OrganisationContext::class)->current()?->name ?? 'No Organisation'
+                    )
+                    ->icon(Heroicon::OutlinedBuildingOffice2)
+                    ->visible(fn () =>
+                        app(OrganisationContext::class)->current() !== null
+                    )
+                    ->disabled(),
+                Action::make('switchOrganisation')
+                    ->label('Switch Organisation')
+                    ->icon(Heroicon::OutlinedBuildingOffice)
+                    ->schema([
+                        Select::make('organisation_id')
+                            ->label('Organisation')
+                            ->options(fn () =>
+                                auth()->user()
+                                    ->organisations
+                                    ->pluck('name', 'id')
+                                )
+                            ->required(),
+                    ])
+                    ->action(function (array $data) {
+                        app(OrganisationContext::class)
+                            ->set(
+                                auth()->user()
+                                    ->organisations()
+                                    ->findOrFail($data['organisation_id'])
+                            );
+                        return redirect(request()->header('Referer'));
+                    })
+                    ->visible(fn () =>
+                        auth()->user()->organisations()->count() > 1
+                    ),
             ]);
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,16 +1,10 @@
 <?php
 
-use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\OrganisationSwitchController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
-});
-
-Route::get('/_debug/clear-org', function () {
-    session()->forget('current_organisation_id');
-
-    return 'Org cleared';
 });
 
 require __DIR__.'/auth.php';
